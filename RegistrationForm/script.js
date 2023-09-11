22c375318fae3e477ae0b95dc86b5de97c460f58
Leenah S <77100372+leenahs@users.noreply.github.com>
2023-09-11 20:46:18 +0300

    new changes

---
+++ RegistrationForm/script.js
@@ -0,0 +1,7 @@
+function openPopup(){
+    document.getElementById("termsPopup").style.display="block";
+}
+
+function closePopup(){
+    document.getElementById("termsPopup").style.display="none";
+}
